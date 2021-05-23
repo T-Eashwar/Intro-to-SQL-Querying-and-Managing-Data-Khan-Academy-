2d02f3a70634ce3acaf0a0fe460a98af8c0fab11
Eashwar T <83827124+T-Eashwar@users.noreply.github.com>
2021-05-23 17:02:20 +0530

    Add files via upload

---
+++ challange1_booklist_database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books ( id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+INSERT INTO books VALUES (1, "hacker's handbook" , 4);
+INSERT INTO books VALUES (2, "blackhat for python" , 5);
+INSERT INTO books VALUES (3, "hack_like_pro" , 3);
+SELECT * FROM books
